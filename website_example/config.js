f202cf6d6bea3c41badc44126211d390ecf350fc
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2018-11-20 17:56:10 +0000

    Added premade configs and example website for FreeHaven

--- website_example/config.js
+++ website_example/config.js
@@ -1,16 +1,16 @@
-var api = "https://yourpool.com:8119";
+var api = "https://freehaven.example.com:8119";
 
-var coinDecimalPlaces = 2;
+var coinDecimalPlaces = 3;
 
 var email = "";
 var telegram = "";
-var discord = "";
+var discord = "https://discord.gg/XAmdwSd";
 
 var marketCurrencies = ["{symbol}-BTC"];
 
-var blockchainExplorer = "https://explorer.ipbc.rocks/block/{id}";
-var transactionExplorer = "https://explorer.ipbc.rocks/tx/{id}";
+var blockchainExplorer = "https://freehaven.coinscope.cc/block/{id}";
+var transactionExplorer = "https://freehaven.coinscope.cc/tx/{id}";
 
-var themeCss = "themes/ipbc.css";
-var themeCssDark = "themes/ipbc_dark.css";
+var themeCss = "themes/freehaven.css";
+var themeCssDark = "themes/freehaven_dark.css";
 var defaultLang = 'en';
