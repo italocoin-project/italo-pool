1c0b78505b978a2bec4a969354d1cee331f143fb
Bafff <26187677+Bafff@users.noreply.github.com>
2019-08-08 01:19:58 +0300

    Change blockchain explorer to explorer.xwp.one

--- website_example/config.js
+++ website_example/config.js
@@ -8,8 +8,8 @@ var discord = "https://discord.gg/XAmdwSd";
 
 var marketCurrencies = ["{symbol}-BTC"];
 
-var blockchainExplorer = "https://swap.coinscope.cc/block/{id}";
-var transactionExplorer = "https://swap.coinscope.cc/tx/{id}";
+var blockchainExplorer = "https://explorer.xwp.one/block/{id}";
+var transactionExplorer = "https://explorer.xwp.one/tx/{id}";
 
 var themeCss = "themes/moon_gray.css";
 var themeCssDark = "themes/moon_gray_dark.css";
